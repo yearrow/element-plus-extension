e85c34ce744f8fbf170726d85c4e12b8d016f061
danielmlc <danielmlc@126.com>
2022-08-19 09:49:49 +0800

    更新打包

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,10 @@ const components = [
 ]
 
 const install = function(Vue:any) {
-  if (install.installed) return
-    install.installed = true
-    // 遍历并注册全局组件
     components.map(component => {
         Vue.component(component.name, component)
   })
 }
-// if (typeof window !== 'undefined' && window.Vue) {
-//   install(window.Vue)
-// }
 
 export default {
   install,
